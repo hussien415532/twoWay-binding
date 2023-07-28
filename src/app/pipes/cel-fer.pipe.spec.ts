c8609e277215e1bb82a5212deeacebcadd812f17
Hussein Okasha <111140118+hussien415532@users.noreply.github.com>
2023-07-28 22:41:15 +0300

    Delete src/app/pipes directory

--- src/app/pipes/cel-fer.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CelFarPipe } from './cel-fer.pipe';
-
-describe('CelFerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CelFarPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
